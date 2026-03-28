2a2a59a07bdb20f4d9b51aea8da0791ee26f86a4
Matt Ayers <73685857+mattayersnz@users.noreply.github.com>
2026-03-28 23:15:15 +0000

    Update hunch formula to 18.4.2-main.1774739701

--- Formula/hunch.rb
+++ Formula/hunch.rb
@@ -1,9 +1,9 @@
 class Hunch < Formula
   desc "Standalone CLI for Hunch note metadata, project context, and curation workflows"
   homepage "https://github.com/mattayersnz/hunch-cli"
-  version "18.4.2-main.1774739587"
-  url "https://github.com/mattayersnz/hunch-cli/releases/download/main-de27199/hunch-18.4.2-main.1774739587-macos-universal.tar.gz"
-  sha256 "64624b5a41d0e280dc00bfb7cb9b0db79eb180ab48ef765d56faab73cc66a104"
+  version "18.4.2-main.1774739701"
+  url "https://github.com/mattayersnz/hunch-cli/releases/download/main-4e61e1e/hunch-18.4.2-main.1774739701-macos-universal.tar.gz"
+  sha256 "73336726853d166dd1390e65d5e36de50465e9f1c06705b2b327a60aba4605ab"
   license "Proprietary"
 
   def install
